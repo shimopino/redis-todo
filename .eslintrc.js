642866be5ffa2be8e26e62c3b7778c966aff5d2b
shimopino <shimoke4869@gmail.com>
2022-11-25 22:11:14 +0900

    テストに必要なライブラリを追加

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/** @type {import('eslint/lib/shared/types').ConfigData} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "neverthrow"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "neverthrow/must-use-result": "error",
+  },
+};
